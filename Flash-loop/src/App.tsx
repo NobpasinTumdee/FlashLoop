1bd1180b9fca31396f086cfcb6425d580db3dbdf
NobpasinTumdee <porporpor579@gmail.com>
2025-07-20 14:52:43 +0700

    set up my project

---
+++ Flash-loop/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>hello flash card</h1>
+    </>
+  )
+}
+
+export default App
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # FlashLoop
+```
+npm install idb
+```
