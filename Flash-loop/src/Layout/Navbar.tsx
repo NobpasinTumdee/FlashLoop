ff3673a8f1d01c171a0d94baa04e298d4de96e2f
NobpasinTumdee <porporpor579@gmail.com>
2025-08-12 22:39:05 +0700

    liquid glass ui navbar

--- Flash-loop/src/Layout/Navbar.tsx
+++ Flash-loop/src/Layout/Navbar.tsx
@@ -1,30 +1,68 @@
 import { Link, useLocation } from "react-router-dom"
 import './Layout.css'
+import GlassSurface from "../component/GlassSurface";
+import { useState } from "react";
 
 const Navbar = () => {
     const location = useLocation();
     const isActive = (path: string) => location.pathname === path;
+    const [Glass, setGlassMode] = useState(false);
     return (
         <>
-            <nav className="Main-nav">
-                <div className="group-nemu">
-                    <Link to="/add" className={`Nenu-nav ${isActive('/add') ? 'active' : ''}`} >
-                        <img height={30} width={30} src="https://webstockreview.net/images/dictionary-clipart-book-index-11.png" alt="Vocabulary" />
-                    </Link>
-                    <Link to="/home" className={`Nenu-nav ${isActive('/home') ? 'active' : ''}`}>
-                        <img height={30} width={30} src="https://static.vecteezy.com/system/resources/previews/022/288/647/non_2x/3d-home-icon-free-png.png" alt="HOME" />
-                    </Link>
-                    <Link to="/review" className={`Nenu-nav ${isActive('/review') ? 'active' : ''}`}>
-                        <img height={30} width={30} src="https://th.bing.com/th/id/R.892c6094d01d82aa1a83000715a97b20?rik=FyaZIUwCxAvDxw&pid=ImgRaw&r=0" alt="flash" />
-                    </Link>
-                    <Link to="/theme" className={`Nenu-nav ${isActive('/theme') ? 'active' : ''}`}>
-                        <img height={30} width={30} src="https://cdn-icons-png.flaticon.com/512/749/749034.png" alt="theme" />
-                    </Link>
-                    <Link to="/achievement" className={`Nenu-nav ${isActive('/achievement') ? 'active' : ''}`}>
-                        <img height={30} width={30} src="https://static.thenounproject.com/png/195251-200.png" alt="theme" />
-                    </Link>
-                </div>
-            </nav>
+            {Glass ? (
+                <nav className="Main-nav">
+                    <GlassSurface
+                        height={"100%"}
+                        borderRadius={50}
+                        width={"auto"}
+                        className="my-custom-class"
+                    >
+                        <div className="group-nemu">
+                            <Link to="/add" className={`Nenu-nav ${isActive('/add') ? 'active' : ''}`} >
+                                <img height={30} width={30} src="https://webstockreview.net/images/dictionary-clipart-book-index-11.png" alt="Vocabulary" />
+                            </Link>
+                            <Link to="/home" className={`Nenu-nav ${isActive('/home') ? 'active' : ''}`}>
+                                <img height={30} width={30} src="https://static.vecteezy.com/system/resources/previews/022/288/647/non_2x/3d-home-icon-free-png.png" alt="HOME" />
+                            </Link>
+                            <Link to="/review" className={`Nenu-nav ${isActive('/review') ? 'active' : ''}`}>
+                                <img height={30} width={30} src="https://th.bing.com/th/id/R.892c6094d01d82aa1a83000715a97b20?rik=FyaZIUwCxAvDxw&pid=ImgRaw&r=0" alt="flash" />
+                            </Link>
+                            <Link to="/theme" className={`Nenu-nav ${isActive('/theme') ? 'active' : ''}`}>
+                                <img height={30} width={30} src="https://cdn-icons-png.flaticon.com/512/749/749034.png" alt="theme" />
+                            </Link>
+                            <Link to="/achievement" className={`Nenu-nav ${isActive('/achievement') ? 'active' : ''}`}>
+                                <img height={30} width={30} src="https://static.thenounproject.com/png/195251-200.png" alt="theme" />
+                            </Link>
+                            <p className="glassMode" onClick={() => setGlassMode(!Glass)}>
+                                {Glass ? '🎉' : '✨'}
+                            </p>
+                        </div>
+                    </GlassSurface>
+                </nav>
+            ) : (
+                <nav className="Main-nav">
+                    <div className="group-nemu">
+                        <Link to="/add" className={`Nenu-nav ${isActive('/add') ? 'active' : ''}`} >
+                            <img height={30} width={30} src="https://webstockreview.net/images/dictionary-clipart-book-index-11.png" alt="Vocabulary" />
+                        </Link>
+                        <Link to="/home" className={`Nenu-nav ${isActive('/home') ? 'active' : ''}`}>
+                            <img height={30} width={30} src="https://static.vecteezy.com/system/resources/previews/022/288/647/non_2x/3d-home-icon-free-png.png" alt="HOME" />
+                        </Link>
+                        <Link to="/review" className={`Nenu-nav ${isActive('/review') ? 'active' : ''}`}>
+                            <img height={30} width={30} src="https://th.bing.com/th/id/R.892c6094d01d82aa1a83000715a97b20?rik=FyaZIUwCxAvDxw&pid=ImgRaw&r=0" alt="flash" />
+                        </Link>
+                        <Link to="/theme" className={`Nenu-nav ${isActive('/theme') ? 'active' : ''}`}>
+                            <img height={30} width={30} src="https://cdn-icons-png.flaticon.com/512/749/749034.png" alt="theme" />
+                        </Link>
+                        <Link to="/achievement" className={`Nenu-nav ${isActive('/achievement') ? 'active' : ''}`}>
+                            <img height={30} width={30} src="https://static.thenounproject.com/png/195251-200.png" alt="theme" />
+                        </Link>
+                        <p className="glassMode" onClick={() => setGlassMode(!Glass)}>
+                            {Glass ? '🎉' : '✨'}
+                        </p>
+                    </div>
+                </nav>
+            )}
         </>
     )
 }
